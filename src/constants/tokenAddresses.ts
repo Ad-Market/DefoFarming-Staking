bde27eac4630f2fca6d3c0a8467d61a0ca47d673
shehzad002 <52923030+shehzad002@users.noreply.github.com>
2020-10-26 01:51:52 +0500

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
 //export const defo = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
-export const defov2 = '0x60adcE513E699aF7206AfC22c4020453460B5236'
-export const defoAddress = '0x257EfB2C5d8fA29c4d4D038D756d7542cF8eC523'
+export const defov2 = '0xFCBaf6f9c56074297930034b38425f56BE20b5A2'
+export const defoAddress = '0x04FAb3f8c4F45930De257F249b54BC3237cafE26'
 export const masterChefAddress = '0xd8f18f8AF3246A81034d72c7a629CC0Ae77B2056'
