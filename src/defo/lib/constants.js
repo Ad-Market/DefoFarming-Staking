5c2de46391bbb37702cf8fb0d230bc2aa83a3f15
shehzad002 <52923030+shehzad002@users.noreply.github.com>
2020-10-26 01:58:00 +0500

    Update constants.js

--- src/defo/lib/constants.js
+++ src/defo/lib/constants.js
@@ -26,48 +26,32 @@ export const INTEGERS = {
 
 export const contractAddresses = {
   defo: {
-    3: '0x60adcE513E699aF7206AfC22c4020453460B5236',
+    1: '0xFCBaf6f9c56074297930034b38425f56BE20b5A2',
   },
   masterChef: {
-    3: '0xd8f18f8AF3246A81034d72c7a629CC0Ae77B2056',
+    1: '0xE1c3c53688026BA52D255Aeb3eD5623e88aF4F56',
   },
   weth: {
-    3: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
   },
   xDefo: {
-    3: '0x0bB0Eee52BF3eC87C1cc4c28628F16D7Cd6C1ACd'
+    1: '0xB212b2111f8a2F8123b0ed773C4b41cd6Ec0e4D4'
   }
 }
 
-/*
-SLP Address on mainnet for reference
-==========================================
-0  USDT 0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852
-1  USDC 0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc
-2  DAI  0xa478c2975ab1ea89e8196811f51a7b7ade33eb11
-3  sUSD 0xf80758ab42c3b07da84053fd88804bcb6baa4b5c
-4  COMP 0xcffdded873554f362ac02f8fb1f02e5ada10516f
-5  LEND 0xab3f9bf1d81ddb224a2014e98b238638824bcf20
-6  SNX  0x43ae24960e5534731fc831386c07755a2dc33d47
-7  UMA  0x88d97d199b9ed37c29d846d00d443de980832a22
-8  LINK 0xa2107fa5b38d9bbd2c461d6edf11b11a50f6b974
-9  BAND 0xf421c3f2e695c2d4c0765379ccace8ade4a480d9
-10 AMPL 0xc5be99a02c6857f9eac67bbce58df5572498f40c
-11 YFI  0x2fdbadf3c4d5a8666bc06645b8358ab803996e28
-12 DEFO 0xce84867c3c02b05dc570d0135103d3fb9cc19433
-*/
+
 
 export const supportedPools = [
   {
     pid: 0,
     lpAddresses: {
-      3: '0x60adcE513E699aF7206AfC22c4020453460B5236',
+      1: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     },
     tokenAddresses: {
-      3: '0x60adcE513E699aF7206AfC22c4020453460B5236',
+      1: '0xFCBaf6f9c56074297930034b38425f56BE20b5A2',
     },
     name: 'Defo Rail!',
-    symbol: 'DEFO-DEFO SLP',
+    symbol: 'DEFO-WETH',
     tokenSymbol: 'DEFO',
     icon: '💎',
   },
